39f8dd91d18eab1406870bdb48e03a22b54593c6
David Morrison <drmorr@appliedcomputing.io>
2023-10-16 13:35:07 -0700

    add arbitrary env vars + more fieldRef types

--- fireconfig/types.py
+++ fireconfig/types.py
@@ -11,6 +11,12 @@ class DownwardAPIField(StrEnum):
     UID = "metadata.uid"
     ANNOTATION = "metadata.annotations[{}]"
     LABEL = "metadata.labels[{}]"
+    SERVICE_ACCOUNT_NAME = "spec.serviceAccountName"
+    NODE_NAME = "spec.nodeName"
+    HOST_IP = "status.hostIP"
+    HOST_IPS = "status.hostIPs"
+    POD_IP = "status.podIP"
+    POD_IPS = "status.podIPs"
 
 
 class TaintEffect(StrEnum):
